8760d5e587229507153613c1e6d057f2fd3f1073
kelly <kelly.wu@wustl.edu>
2025-11-17 00:09:39 -0600

    mod9 initial

--- src/module09/_01lists/UsingLists.java
+++ src/module09/_01lists/UsingLists.java
@@ -1,11 +1,25 @@
 package module09._01lists;
+import java.util.List;
+import java.util.ArrayList;
 
 public class UsingLists {
 
 	public static void main(String[] args) {
-		
-		//your code here!!
-		
-		
+
+		// your code here!!
+		List<String> eating = new ArrayList<String>();
+		eating.add("Pizza");
+		eating.add("Burrito");
+		eating.add("Banana");
+		eating.add("Pie");
+		eating.add("food5");
+		System.out.println(eating.size());
+		for (int i = 0; i < eating.size(); i++){
+			System.out.println(eating.get(i));
+		}
+		eating.remove(2);
+		System.out.println(eating.get(2));
+		System.out.println(eating.size());
+
 	}
 }
--- src/module09/_04equality/Point.java
+++ src/module09/_04equality/Point.java
@@ -8,10 +8,57 @@ public class Point {
 	//
 	// declare instance variables here:
 	//
+	private final int x;
+	private final int y;
 	
-	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 	public static void main(String[] args) {
-		
+		Point p1 = new Point(1, 2);
+		Point p2 = new Point(1, 2);
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 
 }
